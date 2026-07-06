e3192e88f9857a05f44563704e43aaac64145bdc
Yogya Chugh <yogyachugh.coder@gmail.com>
2026-07-06 11:33:09 +0530

    Design: Remove decrypt hover and upgrade blog typography

--- script.js
+++ script.js
@@ -18,42 +18,5 @@ document.addEventListener('DOMContentLoaded', () => {
   const elements = document.querySelectorAll('.fade-in');
   elements.forEach(el => observer.observe(el));
 
-  // --- ASCII Decrypt Hover Effect ---
-  const decryptTargets = document.querySelectorAll('.decrypt-target');
-  const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+-=[]{}|;:,.<>?';
-
-  decryptTargets.forEach(target => {
-    // Store original text
-    const originalText = target.innerText;
-    let isDecrypting = false;
-
-    target.addEventListener('mouseenter', () => {
-      if (isDecrypting) return;
-      isDecrypting = true;
-      
-      let iteration = 0;
-      const maxIterations = 15;
-      
-      const scramble = setInterval(() => {
-        target.innerText = originalText
-          .split('')
-          .map((char, index) => {
-            if (char === ' ') return ' ';
-            if (index < (iteration / maxIterations) * originalText.length) {
-              return originalText[index];
-            }
-            return chars[Math.floor(Math.random() * chars.length)];
-          })
-          .join('');
-        
-        iteration++;
-        
-        if (iteration > maxIterations) {
-          clearInterval(scramble);
-          target.innerText = originalText;
-          isDecrypting = false;
-        }
-      }, 30);
-    });
-  });
+// Decrypt logic removed as per user request.
 });
